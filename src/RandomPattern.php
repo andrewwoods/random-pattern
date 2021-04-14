e69371ebd32cfa024f59834029d8c53235d06cb5
Andrew Woods <services@awoods.net>
2021-04-14 00:35:59 -0400

    Add isTokenValid method with
    This method examines one character of the pattern. This will be a key
    part of validating the pattern.

--- src/RandomPattern.php
+++ src/RandomPattern.php
@@ -12,4 +12,41 @@ class RandomPattern {
     public function __construct($pattern) {
     }
 
+    /**
+     * Check if the token from the pattern is valid
+     *
+     * A => Alphabetic characters
+     * C => Clarified Alphabetic characters
+     * D => Digit
+     * X => C, D
+     *
+     * Some characters can be confused for one another, depending upon the font
+     * used. The number one "1" and the lowercase L "l" are a good example.
+     * The Clarified Alphabetic characters remove these characters from full
+     * English alphabetic
+     *
+     * @param $token
+     *
+     * @return bool
+     */
+    public function isTokenValid($token) : bool {
+        if ($token === 'A') {
+            return true;
+        }
+
+        if ($token === 'C') {
+            return true;
+        }
+
+        if ($token === 'D') {
+            return true;
+        }
+
+        if ($token === 'X') {
+            return true;
+        }
+
+        return false;
+    }
+
 }
--- tests/Unit/RandomPatternTest.php
+++ tests/Unit/RandomPatternTest.php
@@ -15,4 +15,59 @@ class RandomPatternTest extends TestCase
         $this->assertEquals('Awoods\RandomPattern\RandomPattern', get_class($obj));
     }
 
+    public function testIsTokenValidWithInvalidCharacter()
+    {
+        $pattern = '';
+        $token = 'Z';
+
+        $randomPattern = new RandomPattern($pattern);
+        $result = $randomPattern->isTokenValid($token);
+
+        $this->assertFalse($result);
+    }
+
+    public function testIsTokenValidWithCharacterA()
+    {
+        $pattern = '';
+        $token = 'A';
+
+        $randomPattern = new RandomPattern($pattern);
+        $result = $randomPattern->isTokenValid($token);
+
+        $this->assertTrue($result);
+    }
+
+    public function testIsTokenValidWithCharacterC()
+    {
+        $pattern = '';
+        $token = 'C';
+
+        $randomPattern = new RandomPattern($pattern);
+        $result = $randomPattern->isTokenValid($token);
+
+        $this->assertTrue($result);
+    }
+
+    public function testIsTokenValidWithCharacterD()
+    {
+        $pattern = '';
+        $token = 'D';
+
+        $randomPattern = new RandomPattern($pattern);
+        $result = $randomPattern->isTokenValid($token);
+
+        $this->assertTrue($result);
+    }
+
+    public function testIsTokenValidWithCharacterX()
+    {
+        $pattern = '';
+        $token = 'X';
+
+        $randomPattern = new RandomPattern($pattern);
+        $result = $randomPattern->isTokenValid($token);
+
+        $this->assertTrue($result);
+    }
+
 }
